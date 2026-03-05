c00deb919b1123133adf1d14d48bb86a06305772
Yamin Shihab <yaminshihab123@gmail.com>
2026-03-05 10:39:27 -0700

    feat: add problem 0

---
+++ src/problem_0000.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    unsigned long long sum = 0;
+    for (unsigned long long int i = 1; i <= 630000; i += 2)
+        sum += i * i;
+    cout << sum << endl;
+
+    return 0;
+}
